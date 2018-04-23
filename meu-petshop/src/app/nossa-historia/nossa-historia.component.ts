3f3502e7b4024ba2b6a2577467c7660009ce8c37
Nathalia <ribeirosnathalia@gmail.com>
2018-04-22 23:20:57 -0300

    mudanças domingo

--- meu-petshop/src/app/nossa-historia/nossa-historia.component.ts
+++ meu-petshop/src/app/nossa-historia/nossa-historia.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nossa-historia.component.css']
 })
 export class NossaHistoriaComponent implements OnInit {
+  exibeHistoria: boolean = true;
 
   constructor() { }
 
   ngOnInit() {
-  }
+      }
+  
+      public lerMais() {
+        this.exibeHistoria = false;
+      }
 
-}
+      public voltar() {
+        this.exibeHistoria = true;
+      }
+      }
+
+
